dd03b298465672d45b529d974ddc313494e07a8f
yishaiz <yishai5555@gmail.com>
2016-08-14 18:32:17 +0300

    b8

--- app/app.component.ts
+++ app/app.component.ts
@@ -2,9 +2,11 @@ import {Component} from '@angular/core';
 import {HeroDetailComponent} from './hero-detail.component';
 import {Hero} from './Hero';
 
+import {heroes} from './hero.service';
+
 @Component({
   selector: 'my-app',
-  directives:[HeroDetailComponent],
+  directives: [HeroDetailComponent],
   styles: [`
   .selected {
     background-color: #CFD8DC !important;
@@ -65,7 +67,7 @@ import {Hero} from './Hero';
     <h1>{{title}}</h1>
     <h2>My Heroes</h2>
       <ul class="heroes">
-      <!--<li *ngFor="let hero of heroes; trackBy:hero?.id" -->
+ 
       <li *ngFor="let hero of heroes; trackBy: trackByHeroes" 
         (click)="onSelect(hero)"
         [class.selected]="hero === selectedHero">
@@ -73,56 +75,54 @@ import {Hero} from './Hero';
         <span class="badge">{{hero.id}}</span>{{hero.name}}
       </li>
      </ul>
-     
-     
-     
-<div *ngIf="selectedHero">
-  <my-hero-detail [hero]="selectedHero"></my-hero-detail>
-    
-    <!--<h2>{{selectedHero.name}} details!</h2>
-    <div>
-        <label>id: </label>{{selectedHero.id}}</div>
-        <div>
-        <label>name: </label>
-        <input [(ngModel)]="selectedHero.name"  placeholder="name"/>
-    </div>-->
-</div>
+   
+    <div *ngIf="selectedHero">
+      <my-hero-detail [hero]="selectedHero"></my-hero-detail>
+    </div>
 
 `
 })
 export class AppComponent {
   title = 'Tour of Heroes';
 
-  selectedHero:Hero;
+  heroesFromService: Hero[];
+
+  selectedHero: Hero;
+
+  constructor() {
+    this.heroesFromService = heroes;
+  }
 
 
-  //const
-  heroes:Hero[] = [
-    {id: 11, name: 'Mr. Nice'},
-    {id: 12, name: 'Narco'},
-    {id: 13, name: 'Bombasto'},
-    {id: 14, name: 'Celeritas'},
-    {id: 15, name: 'Magneta'},
-    {id: 16, name: 'RubberMan'},
-    {id: 17, name: 'Dynama'},
-    {id: 18, name: 'Dr IQ'},
-    {id: 19, name: 'Magma'},
-    {id: 20, name: 'Tornado'}
-  ];
+  /*
+   //const
+   heroes:Hero[] = [
+   {id: 11, name: 'Mr. Nice'},
+   {id: 12, name: 'Narco'},
+   {id: 13, name: 'Bombasto'},
+   {id: 14, name: 'Celeritas'},
+   {id: 15, name: 'Magneta'},
+   {id: 16, name: 'RubberMan'},
+   {id: 17, name: 'Dynama'},
+   {id: 18, name: 'Dr IQ'},
+   {id: 19, name: 'Magma'},
+   {id: 20, name: 'Tornado'}
+   ];
+   */
 
   // public heroes=HEROS;
 
-  hero:Hero = {
+  hero: Hero = {
     id: 1,
     name: 'Windstorm'
   };
 
-  onSelect(hero:Hero) {
+  onSelect(hero: Hero) {
     this.selectedHero = hero;
     console.log(this.selectedHero);
   }
 
-  trackByHeroes(index: number, hero: Hero){
+  trackByHeroes(index: number, hero: Hero) {
     return hero.id;
   }
 
